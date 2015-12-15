b2561c159b9ccde22f0a47757d00c35b50cb10a3
kent1 <kent1@arscenic.info>
2015-12-15 21:30:04 +0100

    Pour installer nginx, arrêter apache2

--- nginx/install.sh
+++ nginx/install.sh
@@ -6,8 +6,15 @@ source res/check.sh
 # install nginx
 if [[ -z "$VERBOSE" ]]; then
         echo -n "Install nginx: "
+        if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
+			APACHE="running"
+			sudo service apache2 stop
+		fi
         sudo apt-get -y install nginx &>> $LOG_FILE
         check
+        if [[ ! -z $APACHE ]]; then
+        	sudo service apache2 stop
+    	fi
 else
         sudo apt-get -y install nginx 2>&1 | tee -a $LOG_FILE
         echo "Install nginx: `check`"

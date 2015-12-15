bdda06b50dc295388a5692cc8aa66d9e3b667cba
kent1 <kent1@arscenic.info>
2015-12-15 21:30:25 +0100

    Forcer bash...
    Un peu de logs

--- kalite/install.sh
+++ kalite/install.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
 URL=https://github.com/learningequality/ka-lite
 ROOT=/home/ideasbox/Scripts/ka-lite
 CURRENT=$PWD
+LOG_FILE=install.log
 
 # Clone repository
 if [ ! -d "$ROOT" ]
 then
-    echo "Cloning $URL"
-    git clone --recursive $URL $ROOT
+	echo "KA Lite installation"
+	echo "KA Lite installation" &>> $LOG_FILE
+	if [ -z "$VERBOSE" ]; then
+    	echo "Cloning $URL"
+	fi
+	echo "Cloning $URL" &>> $LOG_FILE
+    git clone --recursive $URL $ROOT &>> $LOG_FILE
 fi
 
 cd $ROOT
